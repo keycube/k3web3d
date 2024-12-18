edb51edf82b160350f5dbb29036a271d3bd52dc4
nolannmoisis <97702674+nolannmoisis@users.noreply.github.com>
2024-12-18 08:33:05 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    server: {
-        open: true, // Ouvre le navigateur automatiquement quand vous lancez `npx vite`
-        logLevel: 'info', // Affiche les messages d'info dans la console
-    }
-});
